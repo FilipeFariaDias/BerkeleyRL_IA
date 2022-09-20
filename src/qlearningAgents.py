48b4abfb8eccbb42447ae97339ff4cbaa327c2d1
FilipeFariaDias <filipefariadias@hotmail.com>
2022-09-20 18:57:29 -0300

    Answer Q5 and Q6

--- src/qlearningAgents.py
+++ src/qlearningAgents.py
@@ -112,7 +112,13 @@ class QLearningAgent(ReinforcementAgent):
         legalActions = self.getLegalActions(state)
         action = None
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if len(legalActions) != 0:
+          if util.flipCoin(self.epsilon):
+            action = random.choice(legalActions)
+          else:
+            action = self.computeActionFromQValues(state)
+        else:
+          return None
 
         return action
 
